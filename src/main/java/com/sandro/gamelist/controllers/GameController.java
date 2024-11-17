a4e41fc24fc9b4e07d25687d89c115281ba28612
Sandro <sandrosilva9029@gmail.com>
2024-11-16 23:52:09 -0300

    getr game by id

--- src/main/java/com/sandro/gamelist/controllers/GameController.java
+++ src/main/java/com/sandro/gamelist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.sandro.gamelist.controllers;
 
+import com.sandro.gamelist.dto.GameDTO;
 import com.sandro.gamelist.dto.GameMinDTO;
 import com.sandro.gamelist.entities.Game;
 import com.sandro.gamelist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,11 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll(){
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        return gameService.findById(id);
     }
 
 }
--- src/main/java/com/sandro/gamelist/services/GameService.java
+++ src/main/java/com/sandro/gamelist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.sandro.gamelist.services;
 
+import com.sandro.gamelist.dto.GameDTO;
 import com.sandro.gamelist.dto.GameMinDTO;
 import com.sandro.gamelist.entities.Game;
 import com.sandro.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -14,9 +16,16 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        var result = gameRepository.findById(id);
+        return new GameDTO(result.orElseGet(Game::new));
     }
 
 }
